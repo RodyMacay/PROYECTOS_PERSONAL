d6739e711c2e219346e9b2559662e9f6955ebb5a
Rody Macay <rodymacay123@gmail.com>
2023-08-05 23:35:32 -0500

    priimer Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Juego from "./Juego";
+function App() {
+  
+
+  return (
+    <div className="text-center font-archivo  bg-imagen min-h-screen">
+      <Juego/>
+      
+    </div>
+  );
+}
+
+export default App;
